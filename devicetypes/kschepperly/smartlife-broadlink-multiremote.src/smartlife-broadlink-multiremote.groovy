a99a47e7f833dfa10e53dfef1ee9936f8b2343f2
Keith Schepperly <keith.schepperly@live.com>
2017-07-22 14:39:48 -0400

    .

--- devicetypes/kschepperly/smartlife-broadlink-multiremote.src/smartlife-broadlink-multiremote.groovy
+++ devicetypes/kschepperly/smartlife-broadlink-multiremote.src/smartlife-broadlink-multiremote.groovy
@@ -35,8 +35,8 @@ tiles (scale: 2) {
 valueTile("btn_main", "device.btn_main", width: 2, height: 2) { 
 state "val", label:"", defaultState: true, icon: "http://i96.photobucket.com/albums/l181/kcs317/SmartThings/Transparent.png"
 }
-valueTile("lbl_Soundbar", "device.lbl_Soundbar", width: 6, height: 1) { 
-state "val", label:"JBL Cinema Base", icon: "http://i96.photobucket.com/albums/l181/kcs317/SmartThings/Transparent.png", backgroundColor: "#ffffff"
+standardTile("lbl_Soundbar", "device.lbl_Soundbar", width: 6, height: 1, decoration: "flat") { 
+state "default", label:"JBL Cinema Base", icon: "http://i96.photobucket.com/albums/l181/kcs317/SmartThings/Transparent.png", backgroundColor: "#ffffff"
 }
 standardTile("btn_1", "device.btn_1", width: 1, height: 1, decoration: "flat", canChangeIcon: true, canChangeBackground: true) {
 state "default", label: "Power", action: "Push1", icon: "http://i96.photobucket.com/albums/l181/kcs317/SmartThings/Power.png", backgroundColor: "#ffffff"
