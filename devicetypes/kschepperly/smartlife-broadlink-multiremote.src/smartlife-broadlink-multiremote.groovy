532816358fc6d97882838510e128c5934b45b25b
Keith Schepperly <keith.schepperly@live.com>
2017-07-22 14:31:53 -0400

    .

--- devicetypes/kschepperly/smartlife-broadlink-multiremote.src/smartlife-broadlink-multiremote.groovy
+++ devicetypes/kschepperly/smartlife-broadlink-multiremote.src/smartlife-broadlink-multiremote.groovy
@@ -36,7 +36,7 @@ valueTile("btn_main", "device.btn_main", width: 2, height: 2) {
 state "val", label:"", defaultState: true, icon: "st.Home.home30"
 }
 valueTile("lbl_Soundbar", "device.lbl_Soundbar", width: 6, height: 1) { 
-state "val", label:"JBL Cinema Base", defaultState: true, backgroundColor: "#ffffff"
+state "val", label:"JBL Cinema Base", backgroundColor: "#ffffff"
 }
 standardTile("btn_1", "device.btn_1", width: 1, height: 1, decoration: "flat", canChangeIcon: true, canChangeBackground: true) {
 state "default", label: "Power", action: "Push1", icon: "http://i96.photobucket.com/albums/l181/kcs317/SmartThings/Power.png", backgroundColor: "#ffffff"
