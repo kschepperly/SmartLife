e628668422ab52ed1f9f94387b2d67679831a04e
Keith Schepperly <keith.schepperly@live.com>
2017-07-22 14:06:26 -0400

    .

--- devicetypes/kschepperly/smartlife-broadlink-multiremote.src/smartlife-broadlink-multiremote.groovy
+++ devicetypes/kschepperly/smartlife-broadlink-multiremote.src/smartlife-broadlink-multiremote.groovy
@@ -36,7 +36,7 @@ valueTile("btn_main", "device.btn_main", width: 2, height: 2) {
 state "val", label:"", defaultState: true, icon: "st.Home.home30"
 }
 standardTile("btn_1", "device.btn_1", width: 1, height: 1, canChangeIcon: true, canChangeBackground: true) {
-state "default", label: "Power", action: "Push1", icon: "st.Home.home30", backgroundColor: "#ff0000"
+state "default", label: "Power", action: "Push1", icon: "http://i96.photobucket.com/albums/l181/kcs317/SmartThings/Power.png", backgroundColor: "#ff0000"
 }
 standardTile("btn_2", "device.btn_2", width: 1, height: 1, canChangeIcon: true, canChangeBackground: true) {
 state "default", label: 'Guide', action: "Push2", icon: "st.Home.home30", backgroundColor: "#ff0000"
